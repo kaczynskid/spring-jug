74dc86d350acfcb6b50890879124eb97020db0a6
Dariusz Kaczynski <dariusz.kaczynski@impaqgroup.com>
2013-12-17 22:32:58 +0100

    Simple REST web service configurable with YAML

--- src/main/java/org/jug/boot/Application.java
+++ src/main/java/org/jug/boot/Application.java
@@ -2,15 +2,32 @@ package org.jug.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableConfigurationProperties(Application.Settings.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @ConfigurationProperties(name = "settings")
+    public static class Settings {
+
+        private String greeting;
+
+        public void setGreeting(String greeting) {
+            this.greeting = greeting;
+        }
+
+        public String getGreeting() {
+            return greeting;
+        }
     }
 }
--- src/main/java/org/jug/boot/HelloJug.java
+++ src/main/java/org/jug/boot/HelloJug.java
@@ -1,14 +1,22 @@
 package org.jug.boot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloJug {
 
+    private final Application.Settings settings;
+
+    @Autowired
+    public HelloJug(Application.Settings settings) {
+        this.settings = settings;
+    }
+
     @RequestMapping("/hello")
     public String greet() {
-        return "Hello Jug!";
+        return settings.getGreeting() + " Jug!";
     }
 
 }
