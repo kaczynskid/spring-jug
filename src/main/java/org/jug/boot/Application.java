2ed17a75f41fded0665e1a4de5f970a866e9ac74
Dariusz Kaczynski <dariusz.kaczynski@impaqgroup.com>
2013-12-18 00:26:08 +0100

    Simple REST wen service deployable as regular WAR on standard container

--- src/main/java/org/jug/boot/Application.java
+++ src/main/java/org/jug/boot/Application.java
@@ -2,8 +2,10 @@ package org.jug.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +13,17 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 @EnableConfigurationProperties(Application.Settings.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+    // Bootstraps application when run from command line with embedded container.
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    // Bootstraps application when run within standard container.
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
     @ConfigurationProperties(name = "settings")
